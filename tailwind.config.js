b7be428bd62887594a1c447913ece330378382cd
Amakiri Joseph <jamakiri@netchexonline.com>
2025-07-04 19:50:19 +0100

    feat: setup tailwind and shadcn ui

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./app/**/*.{js,ts,jsx,tsx,mdx}", "./public/index.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
